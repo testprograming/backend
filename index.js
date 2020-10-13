2a3b9e6abb9dfd475c50ddd00732d3aedc20a3e8
jujulili1 <tyulianto@gmail.com>
2020-10-13 14:38:44 +0700

    fix database

--- index.js
+++ index.js
@@ -1,25 +1,15 @@
 const express = require('express');
-// const cors = require('cors');
-// const ejs = require('ejs');
-
 const { db } = require('./config');
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-// app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.set('view engine', 'ejs');
 
-// app.get('/', (req, res) => {
-//     res.render('pages/home');
-// });
 
 app.use('/api/customer', require('./routes/customer'));
-// app.use('/api/customer', require('./routes/customer'));
-// app.use('/api/movie', require('./routes/movie'));
 app.get('*', (req, res) => {
     res.send('404 Not Found');
 });
