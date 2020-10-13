a4ff9edbe59e1ef65901c4b424af4b9e6bf3e8a7
jujulili1 <tyulianto@gmail.com>
2020-10-13 15:55:16 +0700

    add cors

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const { db } = require('./config');
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
